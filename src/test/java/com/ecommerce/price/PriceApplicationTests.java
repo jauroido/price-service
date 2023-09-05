3a68eb9d323d7683cf91c5967f8153b462e1a4c1
jauroido <jaumeroig60@hotmail.com>
2023-09-05 22:30:23 +0200

    Add files via upload

---
+++ src/test/java/com/ecommerce/price/PriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
